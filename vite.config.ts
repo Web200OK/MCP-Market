f584674111ff476a539ad137b33ff870c56e958b
Web200OK <1691929357@qq.com>
2025-04-07 14:16:12 +0800

    新增远程接口代理服务器配置，解决跨域问题

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,21 @@ export default defineConfig({
       enable: true
     })
   ],
+  server: {
+    proxy: {
+      '/server': {
+        target: 'https://919f4618262a6dc3de745e77eed9474a.serveo.net',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/server/, ''),
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
